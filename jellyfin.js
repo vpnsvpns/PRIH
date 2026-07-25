a5ac6494e002acd92d97e5961fca8b57bf865cd2
Prih <y4t7p76f6x@privaterelay.appleid.com>
2026-07-25 13:13:05 +0400

    Update jellyfin.js

--- jellyfin.js
+++ jellyfin.js
@@ -3,7 +3,7 @@
 
     Lampa.Platform.tv();
 
-    var CONFIG = {
+    const CONFIG = {
         host: 'https://ru.mir-kino.pp.ru',
         username: 'rrrrrrrggsloooo@gmail.com',
         password: 'DimaPolina2905',
@@ -12,11 +12,11 @@
         version: '1.0.0'
     };
 
-    var authData = { token: null, userId: null };
+    let authData = { token: null, userId: null };
 
     function getAuthHeader() {
-        var header = 'MediaBrowser Client="' + CONFIG.clientName + '", Device="Lampa", DeviceId="' + CONFIG.deviceId + '", Version="' + CONFIG.version + '"';
-        if (authData.token) header += ', Token="' + authData.token + '"';
+        let header = `MediaBrowser Client="${CONFIG.clientName}", Device="Lampa", DeviceId="${CONFIG.deviceId}", Version="${CONFIG.version}"`;
+        if (authData.token) header += `, Token="${authData.token}"`;
         return header;
     }
 
@@ -24,7 +24,7 @@
         if (authData.token && authData.userId) return callback(true);
 
         $.ajax({
-            url: CONFIG.host + '/Users/AuthenticateByName',
+            url: `${CONFIG.host}/Users/AuthenticateByName`,
             type: 'POST',
             contentType: 'application/json',
             headers: { 'X-Emby-Authorization': getAuthHeader() },
@@ -46,7 +46,7 @@
         authenticate(function (ok) {
             if (!ok) return callback([]);
 
-            var url = CONFIG.host + '/Users/' + authData.userId + '/Items?SearchTerm=' + encodeURIComponent(query) + '&Recursive=true&IncludeItemTypes=Movie,Series,Episode';
+            const url = `${CONFIG.host}/Users/${authData.userId}/Items?SearchTerm=${encodeURIComponent(query)}&Recursive=true&IncludeItemTypes=Movie,Series,Episode`;
 
             $.ajax({
                 url: url,
@@ -58,142 +58,77 @@
         });
     }
 
-    // Главный компонент отображения содержимого Мир Кино
-    function MirKinoComponent(object) {
-        var comp = this;
-        var network = new Lampa.Reguest();
-        var scroll = new Lampa.Scroll({ mask: true, over: true });
-        var files = new Lampa.Explorer(object);
-        var filter = new Lampa.Filter(object);
+    function openMirKino(movie) {
+        const title = movie.title || movie.name;
+        Lampa.Loading.start(function () { Lampa.Loading.stop(); });
 
-        this.start = function () {
-            var title = object.search || object.movie.title || object.movie.name;
-            
-            Lampa.Activity.toggle();
-            Lampa.Loading.start(function () {
-                network.clear();
-                Lampa.Activity.backward();
-            });
+        searchMedia(title, function (items) {
+            Lampa.Loading.stop();
 
-            searchMedia(title, function (items) {
-                Lampa.Loading.stop();
-                
-                if (!items || !items.length) {
-                    var empty = Lampa.Template.get('empty', { title: 'Ничего не найдено', text: 'На Мир Кино нет данного тайтла' });
-                    scroll.append(empty);
-                } else {
-                    items.forEach(function (item) {
-                        var item_html = Lampa.Template.get('button', {
-                            title: item.Name,
-                            subtitle: item.ProductionYear || 'Jellyfin'
-                        });
+            if (!items || !items.length) {
+                Lampa.Noty.show('На Мир Кино ничего не найдено');
+                return;
+            }
 
-                        item_html.on('hover:enter', function () {
-                            var streamUrl = CONFIG.host + '/Videos/' + item.Id + '/stream.m3u8?static=true&api_key=' + authData.token;
-                            Lampa.Player.play({
-                                title: item.Name,
-                                url: streamUrl
-                            });
-                        });
+            const playlist = items.map(item => ({
+                title: item.Name,
+                url: `${CONFIG.host}/Videos/${item.Id}/stream.m3u8?static=true&api_key=${authData.token}`
+            }));
 
-                        scroll.append(item_html);
-                    });
-                }
-
-                files.appendFiles(scroll.render());
-                files.appendHead(filter.render());
-                Lampa.Controller.enable('content');
-            });
-        };
-
-        this.render = function () {
-            return files.render();
-        };
-
-        this.destroy = function () {
-            network.clear();
-            scroll.destroy();
-            files.destroy();
-        };
+            if (playlist.length === 1) {
+                Lampa.Player.play(playlist[0]);
+            } else {
+                Lampa.Select.show({
+                    title: 'Мир Кино: Выбор серии / файла',
+                    items: playlist,
+                    onSelect: function (selected) {
+                        Lampa.Player.play(selected);
+                    }
+                });
+            }
+        });
     }
 
     function startPlugin() {
-        if (window.mirkino_jellyfin_plugin) return;
-        window.mirkino_jellyfin_plugin = true;
+        if (window.jellyfin_mirkino_installed) return;
+        window.jellyfin_mirkino_installed = true;
 
-        // Регистрируем новый компонент в Lampa
-        Lampa.Component.add('mirkino_online', MirKinoComponent);
-
-        // Перевод интерфейса
-        Lampa.Lang.add({
-            mirkino_title: {
-                ru: "Мир Кино (Jellyfin)",
-                en: "Mir Kino (Jellyfin)",
-                uk: "Мір Кіно (Jellyfin)"
-            }
-        });
-
-        // 1. Интеграция кнопки в карточку фильма (full-start__buttons)
-        function addSourceButton(args) {
-            var render = args.render;
-            var movie = args.movie;
-
-            if (render.find('.button--mirkino').length) return;
-
-            var button = $(
-                '<div class="full-start__button selector button--mirkino">' +
-                    '<svg height="24" viewBox="0 0 24 24" width="24" fill="currentColor">' +
-                        '<path d="M12 2L2 22h20L12 2zm0 4.5l6.5 13.5h-13L12 6.5z"/>' +
-                    '</svg>' +
-                    '<span>Мир Кино</span>' +
-                '</div>'
-            );
-
-            button.on('hover:enter click', function () {
-                Lampa.Activity.push({
-                    title: Lampa.Lang.translate('mirkino_title'),
-                    component: 'mirkino_online',
-                    movie: movie,
-                    search: movie.title || movie.name,
-                    page: 1
-                });
-            });
-
-            render.find('.full-start__buttons').append(button);
-        }
-
-        // Подписываемся на события карточки фильма
+        // 1. Встраивание кнопки во все возможные блоки карточки
         Lampa.Listener.follow('full', function (e) {
             if (e.type === 'complite') {
-                addSourceButton({
-                    render: e.object.activity.render(),
-                    movie: e.data.movie
-                });
+                const render = e.object.activity.render();
+                const movie = e.data.movie;
+
+                // Вставляем кнопку под торренты/онлайн или рядом со "Смотреть"
+                let target = render.find('.full-start__buttons');
+                if (!target.length) target = render.find('.full-start-new__buttons');
+
+                if (target.length && !target.find('.button--mirkino').length) {
+                    const btn = $(`
+                        <div class="full-start__button selector button--mirkino" style="background: rgba(255,255,255,0.1); margin-right: 10px;">
+                            <svg height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
+                                <path d="M12 2L2 22h20L12 2zm0 4.5l6.5 13.5h-13L12 6.5z"/>
+                            </svg>
+                            <span>Мир Кино</span>
+                        </div>
+                    `);
+
+                    btn.on('hover:enter click', function () {
+                        openMirKino(movie);
+                    });
+
+                    target.prepend(btn);
+                }
             }
         });
 
-        // 2. Интеграция в системный поиск источников (Search API Lampa)
-        Lampa.Search.addSource({
-            title: 'Мир Кино',
-            search: function (object, callback) {
-                searchMedia(object.query, function (items) {
-                    var results = items.map(function (item) {
-                        return {
-                            title: item.Name,
-                            release_date: item.ProductionYear || '',
-                            url: CONFIG.host + '/Videos/' + item.Id + '/stream.m3u8?static=true&api_key=' + authData.token
-                        };
-                    });
-                    callback([{ title: 'Мир Кино', results: results }]);
-                });
-            },
-            onSelect: function (object, resolve) {
-                resolve();
-                Lampa.Player.play({
-                    title: object.element.title,
-                    url: object.element.url
-                });
-            }
+        // 2. Регистрация в меню "Источник" (если вызвана штатная кнопка "Онлайн")
+        Lampa.Component.add('mirkino_view', function (object) {
+            this.start = function () {
+                openMirKino(object.movie);
+            };
+            this.render = function () { return $('<div></div>'); };
+            this.destroy = function () {};
         });
     }
 
